a6a237444eafb6cf5827d929eaf121426baa3abe
k.pochtovaya <k.pochtovaya@itransition.com>
2022-02-13 10:36:00 +0300

    Added github actions

--- karma.conf.js
+++ karma.conf.js
@@ -32,23 +32,12 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--remote-debugging-port=9222'
-        ]
-      }
-    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessCustom'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
